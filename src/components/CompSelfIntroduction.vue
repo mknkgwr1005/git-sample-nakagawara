2a3e34071b90794c7f30c526d04b750911341eb9
Mika Nakagawara <mika.nakagawara@rakus-partners.co.jp>
2022-02-07 15:41:13 +0900

    新規プロジェクト作成/Ex01作成しました

---
+++ src/components/CompSelfIntroduction.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box">
+    <div class="textInput">
+      <div>名前：{{ myName }}</div>
+      <div>年齢：{{ age }}</div>
+      <div>10年後の年齢：{{ age + 10 }}</div>
+      <div>趣味：{{ myHobby }}</div>
+      <a href="https://www.rakus-partners.co.jp/">ラクスパートナーズ</a>
+      <br />
+      <div id="image">
+        <img v-bind:src="image" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class CompSelfIntroduction extends Vue {
+  private myName = "中川原美華";
+  private age = 28;
+  private myHobby = "ゲーム";
+  //   絶対パスで書く publicを現在地として参照している
+  private image = "/img/miffy.jpg";
+}
+</script>
+
+<style scoped>
+div {
+  width: 200px;
+}
+.box {
+  border: solid black 1px;
+}
+img {
+  width: 200px;
+}
+.textInput {
+  text-align: left;
+  /* border: solid 1px; */
+}
+</style>
---
+++ src/views/Exam1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <CompSelfIntroduction></CompSelfIntroduction>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import CompSelfIntroduction from "@/components/CompSelfIntroduction.vue";
+@Component({
+  components: {
+    CompSelfIntroduction,
+  },
+})
+export default class XXXComponent extends Vue {}
+</script>
+
+<style scoped></style>
